8bb48f93e07e42f923986393511f0f87298c9ba3
yaswanthbandela <yaswanth@hurekatek.com>
2024-04-02 15:08:57 +0530

    added host on before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Backup WordPress files and database
 sudo cp -r /var/www/html /var/www/html_backup
-mysqldump -u yash -ppassword wp > /var/www/html_backup/db_backup.sql
+mysqldump -h 44.194.189.122 -u yash -ppassword wp > /var/www/html_backup/db_backup.sql
