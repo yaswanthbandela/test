4ba827c77ee9e3a0fb78bcb2c8ddca2915386089
yaswanthbandela <yaswanth@hurekatek.com>
2024-04-02 17:14:15 +0530

    test1

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Restore WordPress files and database
-sudo cp -r /var/www/html_backup/* /var/www/html/
+#sudo cp -r /var/www/html_backup/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html
 sudo find /var/www/html -type d -exec chmod 755 {} \;
 sudo find /var/www/html -type f -exec chmod 644 {} \;
