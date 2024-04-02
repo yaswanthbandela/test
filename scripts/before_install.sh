be0ebed1057a107727436218f06f9832d6a7bb8b
yaswanthbandela <yaswanth@hurekatek.com>
2024-04-02 18:19:17 +0530

    test7

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Backup WordPress files and database
-sudo mv -r /var/www/html /var/www/html_backup
-sudo rm -rf /var/www/html/*
+sudo cp -r /var/www/html /var/www/html_backup
 mysqldump -h 44.194.189.122 -u yash -ppassword wp1 > /var/www/html_backup/db_backup.sql
