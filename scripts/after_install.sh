6bf727c67fe03aeded0ff4c99e66d6939fba2320
yaswanthbandela <yaswanth@hurekatek.com>
2024-04-02 18:11:29 +0530

    test6

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -8,7 +8,7 @@ sudo find /var/www/html -type f -exec chmod 644 {} \;
 sudo systemctl restart nginx
 
 # Replace wp-config.php with live configuration
-sudo cp /var/www/html/wp-config-live.php /var/www/html/wp-config.php  # Assuming this is your live configuration file
+sudo cp /var/www/html_backup/wp-config-live.php /var/www/html/wp-config.php  # Assuming this is your live configuration file
 sudo chown www-data:www-data /var/www/html/wp-config.php  # Change ownership if needed
 sudo chmod 644 /var/www/html/wp-config.php  # Ensure correct permissions
 
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Backup WordPress files and database
 sudo mv -r /var/www/html /var/www/html_backup
+sudo rm -rf /var/www/html/*
 mysqldump -h 44.194.189.122 -u yash -ppassword wp1 > /var/www/html_backup/db_backup.sql
